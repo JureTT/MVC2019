4f4b2cebc8bc659d7772a519de6f37aaeacb2486
JureTT <jureperic@gmail.com>
2019-04-11 18:22:02 +0200

    komit entityframework

---
+++ Adresar/Controllers/KontaktController.cs
@@ -0,0 +1,108 @@
+﻿using Adresar.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Adresar.Controllers
+{
+    public class KontaktController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Kontakt
+        public ActionResult Index()
+        {
+            List<Kontakt> aktivniKontakti = (from k in _db.Kontakti
+                                             where k.Aktivan select k).ToList();
+            return View(aktivniKontakti);
+        }
+        // GET: Kontakt/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // POST: Kontakt/Create
+        [HttpPost]
+        public ActionResult Create(Kontakt kontakt)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Kontakti.Add(kontakt);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kontakt);
+        }
+        // GET: Kontakt/Details/id
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kontakt kontakt = _db.Kontakti.Find(id);
+            if (kontakt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kontakt);
+        }
+        // GET: Kontakt/Edit/id
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kontakt kontakt = _db.Kontakti.Find(id);
+            if (kontakt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kontakt);
+        }
+        // POST: Kontakt/Edit/id
+        [HttpPost]
+        public ActionResult Edit(Kontakt kontakt)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(kontakt).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kontakt);
+        }
+        // GET: Kontakt/Delete/id
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kontakt kontakt = _db.Kontakti.Find(id);
+            if (kontakt == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kontakt);
+        }
+        // POST: Kontakt/Delete/id
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kontakt kontakt = _db.Kontakti.Find(id);
+            _db.Kontakti.Remove(kontakt);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
+    }
+}
