4619c50af796e5aa806252c93dc939eca48581a4
Jure <jureperic@gmail.com>
2019-04-15 15:11:31 +0200

    webshop

---
+++ WebShop/Controllers/WebShopController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using WebShop.Models;
+
+namespace WebShop.Controllers
+{
+    public class WebShopController : Controller
+    {
+        // GET: WebShop
+        private WebShopEntities db = new WebShopEntities();
+
+        public ActionResult Index()
+        {
+            var proizvodi = db.Proizvodis.Include(p => p.MjereProizvoda);
+            return View(proizvodi.ToList());
+        }
+    }
+}
